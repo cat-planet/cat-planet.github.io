10a622f4e573eb31374e38154ed6ef02c2d2d7a6
scientificworld <scientificworld@126.com>
2020-09-15 22:49:37 +0800

    .

---
+++ cat_planet/init.js
@@ -0,0 +1,9 @@
+var id = "4mh77VlY9Bvd8mYNHYwxJykB-gzGzoHsz";
+var key = "myyaTg5fXgrzHSeEQXw8bCb7";
+var rest = "https://4mh77vly.lc-cn-n1-shared.com"
+
+AV.init({
+	appId: id,
+	appKey: key,
+	serverURL: rest
+});
